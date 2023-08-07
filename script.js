390a0ab87fc1945d27cc5bd08b090ff73d4ea2a1
m1rojke <mirojkeeeee@gmail.com>
2023-08-07 23:51:43 +0300

    create setInterval

--- script.js
+++ script.js
@@ -1,15 +1,36 @@
 const text = document.getElementById("text");
 const title = document.querySelector(".title");
+let intervalId;
 
-text.addEventListener("click", () => {
+function setter() {
+  if (!intervalId) {
+    intervalId = setInterval(changeColor, 1000);
+  }
+}
+
+function offSetter() {
+  clearInterval(intervalId);
+  intervalId = null;
+}
+
+function changeColor() {
   if (title.style.backgroundColor == "" && text.style.color == "") {
     title.style.backgroundColor = "black";
     text.style.color = "white";
-  } else if (title.style.backgroundColor == "black" && text.style.color == "white") {
+  } else if (
+    title.style.backgroundColor == "black" &&
+    text.style.color == "white"
+  ) {
     title.style.backgroundColor = "white";
     text.style.color = "black";
-  } else if (title.style.backgroundColor == "white" && text.style.color == "black") {
+  } else if (
+    title.style.backgroundColor == "white" &&
+    text.style.color == "black"
+  ) {
     title.style.backgroundColor = "black";
     text.style.color = "white";
   }
-});
+}
+
+text.addEventListener("click", setter);
+text.addEventListener("dblclick", offSetter);
